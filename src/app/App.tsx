98f5f0021bb548eb1bf83f1b5e2a5c0ff5f1dafe
cnandza <cnandza@saudigitus.org>
2025-05-14 14:45:54 +0200

    lin

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,11 @@
 import React from 'react'
 import './App.module.css'
 import { Router } from '../components/routes'
-import { AppWrapper } from 'dhis2-semis-components'
 
 const MyApp = () => {
 
     return (
-        <AppWrapper dataStoreKey='dataStore/semis/values'>
-            <Router />
-        </AppWrapper>
+        <Router />
     )
 }
 
--- src/components/routes/Router.tsx
+++ src/components/routes/Router.tsx
@@ -1,18 +1,16 @@
 import React from 'react';
 import { Reenrollment } from '../../pages';
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import WithHeaderBarLayout from '../../layout/WithHeaderBarLayout';
 
 export default function Router() {
     return (
-        <HashRouter>
-            <Routes>
-                <Route path='/'
-                    element={<WithHeaderBarLayout />}
-                >
-                    <Route key={'re-enroll'} path={'/'} element={<Reenrollment />} />
-                </Route>
-            </Routes>
-        </HashRouter>
+        <Routes>
+            <Route path='/'
+                element={<WithHeaderBarLayout />}
+            >
+                <Route key={'re-enroll'} path={'/'} element={<Reenrollment />} />
+            </Route>
+        </Routes>
     );
 }
